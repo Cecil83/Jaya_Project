4f033970068f15b4b3cbdee7537e43b95958c42c
Ben <benoitpares@gmail.com>
2022-11-25 17:31:57 +0100

    Ajout du fichier plan_d'experience Ce script itère sur les crops, soils, suns, temperatures, et stocke les résultats de WOFOST associés

--- Project_JAYA/wofost_curves/config/config.py
+++ Project_JAYA/wofost_curves/config/config.py
@@ -17,16 +17,16 @@ Choix de crops :
 """
 # Soit itérer sur toutes les crops
 
-#one_crop=False
-one_crop=True
+one_crop=False
+#one_crop=True
 
 # Si one_crop=True
 crop_name = 'fababean'
 
 # CONFIG
 
-start_date = datetime.datetime(2006, 1, 1, 0, 0)
-end_date = datetime.datetime(2007, 1, 1, 0, 0)
+start_date = datetime.date(2006, 1, 1)
+end_date = datetime.date(2007, 1, 1)
 
 # WEATHER
 # Soleil
@@ -60,8 +60,8 @@ temp = 18.
 
 # Soit itérer sur tous les soils
 
-#one_soil=False
-one_soil=True
+one_soil=False
+#one_soil=True
 
 # Si one_soil=True
 soil_name = 'ec3.soil'
@@ -79,27 +79,27 @@ soil_name = 'ec3.soil'
 # Soleil, température, eau, nutriment
 
 crop_dict = {'sunflower': Crop(name='sunflower', variety='Sunflower_1101'),
-             'soybean': Crop(name='soybean', variety='Soybean_901'),
-             'millet': Crop(name='millet', variety='Millet_VanHeemst_1988'),
-             'barley': Crop(name='springbarley', variety='Spring_barley_301'),
-             'mungbean': Crop(name='mungbean', variety='Mungbean_VanHeemst_1988'),
-             'tobacco': Crop(name='tobacco', variety='Tobacco_VanHeemst_1988'),
-             'rapeseed': Crop(name='rapeseed', variety='Oilseed_rape_1001'),
-             'groundnut': Crop(name='groundnut', variety='Groundnut_VanHeemst_1988'),
-             'cotton': Crop(name='cotton', variety='Cotton_VanHeemst_1988'),
-             'cowpea': Crop(name='cowpea', variety='Cowpea_VanHeemst_1988'),
-             'chickpea': Crop(name='chickpea', variety='Chickpea_VanHeemst_1988'),
-             'sugarbeet': Crop(name='sugarbeet', variety='Sugarbeet_601'),
+             #'soybean': Crop(name='soybean', variety='Soybean_901'),
+             #'millet': Crop(name='millet', variety='Millet_VanHeemst_1988'),
+             #'barley': Crop(name='barley', variety='Spring_barley_301'),
+             #'mungbean': Crop(name='mungbean', variety='Mungbean_VanHeemst_1988'),
+             #'tobacco': Crop(name='tobacco', variety='Tobacco_VanHeemst_1988'),
+             #'rapeseed': Crop(name='rapeseed', variety='Oilseed_rape_1001'),
+             #'groundnut': Crop(name='groundnut', variety='Groundnut_VanHeemst_1988'),
+             #'cotton': Crop(name='cotton', variety='Cotton_VanHeemst_1988'),
+             #'cowpea': Crop(name='cowpea', variety='Cowpea_VanHeemst_1988'),
+             #'chickpea': Crop(name='chickpea', variety='Chickpea_VanHeemst_1988'),
+             #'sugarbeet': Crop(name='sugarbeet', variety='Sugarbeet_601'),
              'fababean': Crop(name='fababean', variety='Faba_bean_801'),
-             'sugarcane': Crop(name='sugarcane', variety='Sugarcane_VanHeemst_1988'),
-             'potato': Crop(name='potato', variety='Potato_701'),
-             'wheat': Crop(name='winterwheat', variety='Winter_wheat_101'),
-             'pigeonpea': Crop(name='pigeonpea', variety='Pigeonpea_VanHeemst_1988'),
-             'sorghum': Crop(name='sorghum', variety='Sorghum_VanHeemst_1988'),
-             'cassava': Crop(name='cassava', variety='Cassava_VanHeemst_1988'),
-             'maize': Crop(name='tropical_maize', variety='Maize_VanHeemst_1988'),
+             #'sugarcane': Crop(name='sugarcane', variety='Sugarcane_VanHeemst_1988'),
+             #'potato': Crop(name='potato', variety='Potato_701'),
+             #'wheat': Crop(name='wheat', variety='Winter_wheat_101'),
+             #'pigeonpea': Crop(name='pigeonpea', variety='Pigeonpea_VanHeemst_1988'),
+             #'sorghum': Crop(name='sorghum', variety='Sorghum_VanHeemst_1988'),
+             #'cassava': Crop(name='cassava', variety='Cassava_VanHeemst_1988'),
+             'maize': Crop(name='maize', variety='Maize_VanHeemst_1988'),
              'seed_onion': Crop(name='seed_onion', variety='onion_agriadapt'),
-             'rice': Crop(name='rice_eu', variety='Rice_501'),
+             'rice': Crop(name='rice', variety='Rice_501'),
              'sweetpotato': Crop(name='sweetpotato', variety='Sweetpotato_VanHeemst_1988')}
 
 
@@ -127,12 +127,12 @@ else:
 # Temperature
 
 if one_temp:
-    temps = temp
+    temps = [temp]
 else:
     temps = [temp_min + temp_step*float(i) for i in range(10)]
 
 
-# Reste à faire l'eau et les nutriments
+# Reste à faire l'eau et les nutriments, et le site (soil profile etc )
 
 
 
@@ -155,11 +155,11 @@ weather_config_dict={'irrad': 10000.,
 # Agro File Config
 
 agro_dict = {'Version': 1.0,
-             'AgroManagement': [{datetime.date(2006, 1, 1): {'CropCalendar': {'crop_name': crop_dict[crop_name].name,
+             'AgroManagement': [{start_date: {'CropCalendar': {'crop_name': crop_dict[crop_name].name,
                  'variety_name': crop_dict[crop_name].variety,
-                 'crop_start_date': datetime.date(2006, 4, 5),
+                 'crop_start_date': start_date,
                  'crop_start_type': 'emergence',
-                 'crop_end_date': datetime.date(2006, 10, 20),
+                 'crop_end_date': end_date,
                  'crop_end_type': 'harvest',
                  'max_duration': 300},
                 'TimedEvents': None,
--- Project_JAYA/wofost_curves/plan_dexperience_csv.py
+++ Project_JAYA/wofost_curves/plan_dexperience_csv.py
@@ -5,12 +5,9 @@
 import sys, os
 from config.pcse_path import pcse_path
 sys.path.insert(1, pcse_path)
-import matplotlib
-#matplotlib.style.use("ggplot")
 import matplotlib.pyplot as plt
 import pandas as pd
 import datetime,yaml
-import csv
 
 import config.config as c
 
@@ -29,15 +26,6 @@ results_dir = os.path.join(os.getcwd(), 'results')
 print("python version: %s " % sys.version)
 print("PCSE version: %s" %  pcse.__version__)
 
-# CONFIG
-
-#crop_name = 'mungbean'
-#variety_name = 'Mungbean_VanHeemst_1988'
-crop_name = 'fababean'
-variety_name = 'Faba_bean_801'
-start_date = datetime.datetime(2006, 1, 1, 0, 0)
-end_date = datetime.datetime(2007, 1, 1, 0, 0)
-
 variable_meaning_dict={
     'TWSO': 'Total dry weight of living storage organs (kg ha-1)',
     'LAI': 'Leaf area index: (leaf area)/(soil area) (ha•ha-1)',
@@ -53,18 +41,14 @@ variable_meaning_dict={
 }
 
 
-def get_wofost_parameter_set():
+def get_wofost_parameter_set(crop,soil):
     yaml_repo = os.path.join(data_dir, 'yaml')
     cropd = YAMLCropDataProvider(yaml_repo)
-    #print(cropd)
-    cropd.get_crops_varieties()
-    cropd.set_active_crop(c.crop.name, c.crop.variety)
-    #print(cropd)
+    cropd.set_active_crop(crop.name, crop.variety)
 
-    soilfile = os.path.join(data_dir, 'soil', 'ec3.soil')
+    soilfile = os.path.join(data_dir, 'soil', soil)
     soild = CABOFileReader(soilfile)
-    sited = WOFOST72SiteDataProvider(WAV=10, CO2=360)
-    #print(sited)
+    sited = WOFOST72SiteDataProvider(WAV=10)
     parameters = ParameterProvider(cropdata=cropd, soildata=soild, sitedata=sited)
     return parameters
 
@@ -73,6 +57,9 @@ def get_wofost_parameter_set():
 def prepare_agromanagement(crop):
     # Prepare le fichier agromanagement avec la bonne culture
     agromanagement_file = os.path.join(data_dir, 'agro', 'sugarbeet_calendar.agro')
+    
+    c.agro_dict['AgroManagement'][0][c.start_date]['CropCalendar']['crop_name'] = crop.name
+    c.agro_dict['AgroManagement'][0][c.start_date]['CropCalendar']['variety_name'] = crop.variety
     with open(agromanagement_file, 'w') as file:
         file.flush()
         yaml.dump(c.agro_dict, file)
@@ -138,96 +125,27 @@ def get_result_of_wofost_run(parameters, weather, agromanagement):
     df_results.tail()
     return df_results
 
-def print_curves(df_results):
-    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12,10))
-    for var, ax in zip(["DVS", "TAGP", "LAI", "TWSO"], axes.flatten()):
-        ax.plot_date(df_results.index, df_results[var], 'b-')
-        ax.set_title(var)
-    fig.autofmt_xdate()
-
-def print_var(var_type, df_results):
-    fig, ax = plt.subplots()
-    ax.plot_date(df_results.index, df_results[var_type], 'k-')
-    ax.set_title(f"{var_type} : {variable_meaning_dict[var_type]}")
-    plt.show()
-
-def save_var(var_type, df_results, filename):
-    fig, ax = plt.subplots()
-    ax.plot_date(df_results.index, df_results[var_type], 'k-')
-    ax.set_title(f"{var_type} : {variable_meaning_dict[var_type]}")
-    plt.savefig(filename)
-
-
-def Write_csv_from_Data (filename,path, Data_W):
-    os.chdir(path)
-    f = open(filename, 'w')
-    # create the csv writer
-    writer = csv.writer(f, delimiter=',', lineterminator='\n')
-    # writer.writerow(header)
-    for row in Data_W:
-        writer.writerow(row)
-    f.close()
-
-#def write_to_excel(df_results, )
+def start_runs_for_weather_parameters(parameters,agro,path):
+    for irrad in c.suns:
+        for temperature in c.temps:
+            c.weather_config_dict['irrad'] = irrad
+            c.weather_config_dict['tmin'] = temperature
+            c.weather_config_dict['tmax'] = temperature
+            # modify weather file accordingly
+            tmp_weatherfile = prepare_fictional_weather_file()
+            df_results = get_result_of_wofost_run(parameters, tmp_weatherfile, agro)
+            df_results.to_csv(os.path.join(path,f"irr_{irrad / 1000:.0f}_temp_{temperature:.1f}.csv"))
 
 if __name__=='__main__':
-    parameters = get_wofost_parameter_set()
-    agromanagement = prepare_agromanagement()
-    irrad_list = list()
-    twso_max_list = list()
-    results_dir_now = os.path.join(results_dir, f"TWSO_{crop_name}_{datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}")
-    os.mkdir(results_dir_now)
-    fig1, ax1 = plt.subplots()
-    fig2, ax2 = plt.subplots()
-    fig_dict, ax_dict = dict(), dict()
-    for key in variable_meaning_dict.keys():
-        fig_dict[key], ax_dict[key] = plt.subplots()
-    for i in range(10):
-        new_irrad = float(4000+i*2500)
-
-        irrad_list.append(new_irrad)
-        # modify weather_config_file
-        c.weather_config_dict['irrad'] = new_irrad
-        # modify weather file accordingly
-        tmp_weatherfile = prepare_fictional_weather_file()
-        df_results = get_result_of_wofost_run(parameters, tmp_weatherfile, agromanagement)
-        result_name = os.path.join(results_dir_now, f"TWSO_{crop_name}_irrad_{new_irrad}.png")
-        #save_var('TWSO', df_results, result_name)
-        #save_var('LAI', df_results, os.path.join(results_dir_now, f"LAI_{crop_name}_irrad_{new_irrad}.png"))
-
-        twso_max_list.append(df_results["TWSO"].max())
-        df_results.to_csv(os.path.join(results_dir_now,f"TWSO_{crop_name}_irrad_{new_irrad}.csv"))
-
-        for key in variable_meaning_dict.keys():
-            fig = ax_dict[key].plot(df_results.index, df_results[key], '-', label=f"{new_irrad / 1000:.1f}")
-            ax_dict[key].legend(title=f"Irradiations in MJ/m2/day ")
-            ax_dict[key].set_title(f"{key} : {variable_meaning_dict[key]}")
-            fig_dict[key].savefig(os.path.join(results_dir_now, f"{key}_irrad_fct_temps.png"))
-
-
-        # ax1.plot(df_results.index, df_results[key], '-', label=f"{new_irrad/1000:.1f}")
-        # ax1.legend()
-        # fig1.savefig(os.path.join(results_dir_now, f"{key}_irrad_fct_temps.png"))
-        #
-        # ax2.plot(df_results.index, df_results['LAI'], '-', label=f"{new_irrad/1000:.1f}")
-        # ax2.legend()
-        # fig2.savefig(os.path.join(results_dir_now, f"LAI_irrad_fct_temps.png"))
-        #
-
-    result_name = os.path.join(results_dir_now, f"TWSO_{crop_name}_en_fonction_de_irrad.png")
-    fig, ax = plt.subplots()
-    ax.plot(irrad_list, twso_max_list, 'g-')
-    ax.set_title("TWSO (kg ha-1) en fonction de l'irradiation (kJ/m2/day)")
-    plt.savefig(result_name)
-
-    Data_results, header = [],[]
-
-    Data_results.append([key for key in c.weather_config_dict.keys()])
-    Data_results.append([c.weather_config_dict[key] for key in c.weather_config_dict.keys()])
-
-    Data_results.append(('Irrad_value','TWSO_harvest_value'))
-    for i in range(len(irrad_list)):
-        Data_results.append((irrad_list[i], twso_max_list[i]))
-
-    Write_csv_from_Data(f"Results_{crop_name}_irrad.csv", results_dir_now, Data_results)
+    new_res_dir = os.path.join(results_dir,f"{datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}")
+    os.mkdir(new_res_dir)
+    for soil in c.soils:
+        for crop in c.crops:
+            parameters = get_wofost_parameter_set(crop, soil)
+            agromanagement = prepare_agromanagement(crop)
+            results_dir_now = os.path.join(new_res_dir, f"{soil}_{crop.name}")
+            os.mkdir(results_dir_now)
+            start_runs_for_weather_parameters(parameters, agromanagement, results_dir_now)
+            
+    
 
